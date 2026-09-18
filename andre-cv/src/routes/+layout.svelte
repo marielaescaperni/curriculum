dbb9f42c89270986efc88ef829bf9a0f79a969d2
Mariela Escalante <44972222+marielaescaperni@users.noreply.github.com>
2026-09-17 21:17:23 -0400

    fix(layout): allow sticky case-study navigation

--- andre-cv/src/routes/+layout.svelte
+++ andre-cv/src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 	let { children } = $props();
 </script>
 
-<div class="relative min-h-screen overflow-hidden">
+<div class="relative min-h-screen overflow-x-clip">
 	<LavaLampBackground />
 
 	<div id="top" class="relative z-10 min-h-screen text-primary">
